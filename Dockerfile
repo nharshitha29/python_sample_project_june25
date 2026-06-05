6aba8a0ae7c64e41f67f8367ec1fad0ab7bd9053
Harshitha <nharshitha29@gmail.com>
2026-06-05 15:25:36 +0530

    modified the files

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.13-slim
-WORKDIR /spc
+WORKDIR /app
 COPY . /app
 RUN pip install -r requirements.txt
 EXPOSE 8000
--- app/main.py
+++ app/main.py
@@ -70,3 +70,5 @@ def search_employee(name: str):
     return {
         "message": f"Searching employee: {name}"
     }
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
